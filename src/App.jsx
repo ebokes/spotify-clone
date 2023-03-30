7daf1ddcdd3512fef598695bf664a2ed378fc02b
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2023-03-30 22:51:39 +0100

    feature: authenticate user with token

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
 import Login from "./pages/Login";
+import Spotify from "./pages/Spotify";
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      if (token) {
+        dispatch({ type: reducerCases.SET_TOKEN, token });
+      }
+    }
+  }, [dispatch, token]);
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
 
 export default App;
